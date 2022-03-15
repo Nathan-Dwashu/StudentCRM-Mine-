5fe07c679c8dba083d7215508e35164ee0b98140
Nathan-Dwashu <nathandwashu99@gmail.com>
2022-03-15 12:37:02 +0200

    Initial commit

---
+++ StudentCRM/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace StudentCRM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxStudentNo = new System.Windows.Forms.TextBox();
+            this.txtBoxFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxIDNum = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxEnroll = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cboCampus = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBoxSearch = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dgvStudentsList = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentsList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAdd.Location = new System.Drawing.Point(169, 414);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(91, 33);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(45, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "StudentNo";
+            // 
+            // txtBoxStudentNo
+            // 
+            this.txtBoxStudentNo.Location = new System.Drawing.Point(202, 73);
+            this.txtBoxStudentNo.Name = "txtBoxStudentNo";
+            this.txtBoxStudentNo.ReadOnly = true;
+            this.txtBoxStudentNo.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxStudentNo.TabIndex = 2;
+            // 
+            // txtBoxFirstName
+            // 
+            this.txtBoxFirstName.Location = new System.Drawing.Point(202, 121);
+            this.txtBoxFirstName.Name = "txtBoxFirstName";
+            this.txtBoxFirstName.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxFirstName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(45, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FirstName";
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.Location = new System.Drawing.Point(202, 169);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxLastName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(45, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LastName";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(202, 217);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxEmail.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(45, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // txtBoxIDNum
+            // 
+            this.txtBoxIDNum.Location = new System.Drawing.Point(202, 265);
+            this.txtBoxIDNum.Name = "txtBoxIDNum";
+            this.txtBoxIDNum.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxIDNum.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(45, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 27);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "IDNumber";
+            // 
+            // txtBoxEnroll
+            // 
+            this.txtBoxEnroll.Location = new System.Drawing.Point(284, 313);
+            this.txtBoxEnroll.Name = "txtBoxEnroll";
+            this.txtBoxEnroll.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxEnroll.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(45, 311);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(233, 27);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Enrolledment Date";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(45, 359);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 27);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Campus";
+            // 
+            // cboCampus
+            // 
+            this.cboCampus.FormattingEnabled = true;
+            this.cboCampus.Location = new System.Drawing.Point(181, 361);
+            this.cboCampus.Name = "cboCampus";
+            this.cboCampus.Size = new System.Drawing.Size(121, 23);
+            this.cboCampus.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(307, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(218, 27);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "My Students CRM";
+            // 
+            // txtBoxSearch
+            // 
+            this.txtBoxSearch.Location = new System.Drawing.Point(491, 106);
+            this.txtBoxSearch.Name = "txtBoxSearch";
+            this.txtBoxSearch.Size = new System.Drawing.Size(230, 23);
+            this.txtBoxSearch.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(394, 106);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 27);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Search";
+            // 
+            // dgvStudentsList
+            // 
+            this.dgvStudentsList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentsList.Location = new System.Drawing.Point(403, 136);
+            this.dgvStudentsList.Name = "dgvStudentsList";
+            this.dgvStudentsList.RowTemplate.Height = 25;
+            this.dgvStudentsList.Size = new System.Drawing.Size(412, 261);
+            this.dgvStudentsList.TabIndex = 18;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Lime;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnUpdate.Location = new System.Drawing.Point(295, 414);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(88, 33);
+            this.btnUpdate.TabIndex = 19;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.DarkRed;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnDelete.Location = new System.Drawing.Point(418, 414);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(80, 33);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnClear.Location = new System.Drawing.Point(533, 414);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(81, 33);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(705, 13);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 81);
+            this.btnExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnExit.TabIndex = 23;
+            this.btnExit.TabStop = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.MouseHover += new System.EventHandler(this.btnExit_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 470);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvStudentsList);
+            this.Controls.Add(this.txtBoxSearch);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cboCampus);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBoxEnroll);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBoxIDNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxStudentNo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentsList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxStudentNo;
+        private System.Windows.Forms.TextBox txtBoxFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxIDNum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxEnroll;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cboCampus;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBoxSearch;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dgvStudentsList;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox btnExit;
+    }
+}
+
---
+++ StudentCRM/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentCRM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if ( result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnExit_MouseHover(object sender, EventArgs e)
+        {
+            btnExit.BackColor = Color.Blue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("It loads beautifily");
+        }
+    }
+}
